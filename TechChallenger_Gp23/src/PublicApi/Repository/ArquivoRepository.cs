21e999dcd8ba86b1e0d715121bc9fced7b95f87a
karina felix <felix_karina00@hotmail.com>
2023-07-01 19:34:05 -0300

    get Imgs

--- TechChallenger_Gp23/src/PublicApi/Repository/ArquivoRepository.cs
+++ TechChallenger_Gp23/src/PublicApi/Repository/ArquivoRepository.cs
@@ -27,12 +27,12 @@ namespace PublicApi.Repository
             using (var cn = new SqlConnection(connection))
             {
 
-                //var arquivos = cn.Query<Arquivo>("SELECT * FROM TBImagem");
-                var arquivos = new List<Arquivo>();
-                arquivos.Add(new Arquivo() { Id = 1, Nome = "Teste", URL = "teste.png" });
-                arquivos.Add(new Arquivo() { Id = 2, Nome = "Teste", URL = "teste.png" });
-                arquivos.Add(new Arquivo() { Id = 3, Nome = "Teste", URL = "teste.png" });
-                arquivos.Add(new Arquivo() { Id = 4, Nome = "Teste", URL = "~/techChallengeIcon.png" });
+                var arquivos = cn.Query<Arquivo>("SELECT * FROM TBImagem");
+                //var arquivos = new List<Arquivo>();
+                //arquivos.Add(new Arquivo() { Id = 1, Nome = "Teste", URL = "teste.png" });
+                //arquivos.Add(new Arquivo() { Id = 2, Nome = "Teste", URL = "teste.png" });
+                //arquivos.Add(new Arquivo() { Id = 3, Nome = "Teste", URL = "teste.png" });
+                //arquivos.Add(new Arquivo() { Id = 4, Nome = "Teste", URL = "~/techChallengeIcon.png" });
 
                 return arquivos;
             }
