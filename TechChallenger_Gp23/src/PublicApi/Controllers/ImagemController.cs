4e4effc7d14230158105e91027232b543e0bf7eb
karina felix <felix_karina00@hotmail.com>
2023-07-03 19:00:26 -0300

    Ajuste

--- TechChallenger_Gp23/src/PublicApi/Controllers/ImagemController.cs
+++ TechChallenger_Gp23/src/PublicApi/Controllers/ImagemController.cs
@@ -4,36 +4,15 @@ using PublicApi.Repository;
 
 namespace PublicApi.Controllers
 {
-   // [ApiController]
     [Route("[controller]")]
     public class ImagemController : ControllerBase
     {
         private  readonly IArquivoRepository _repository;
-        private readonly ILogger<ImagemController> _logger;
-
-        public ImagemController(IArquivoRepository repository, ILogger<ImagemController> logger)
+        
+        public ImagemController(IArquivoRepository repository)
         {
             _repository = repository;
-            _logger = logger;
         }
-
-        //private static readonly string[] Summaries = new[]
-        //{
-        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        //};
-
-
-        //[HttpGet("GetWeatherForecast")]
-        //public IEnumerable<WeatherForecast> GetWeatherForecast()
-        //{
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
 
         [HttpGet("ConsultaImagens")]
         public IEnumerable<Arquivo> ConsultaImagens()
